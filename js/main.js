b823cff6b2833b6a4ab2854deb9e764e94d0e858
Nando5 <flucero7@gmail.com>
2018-04-16 22:30:15 +1000

    refactored main.js

--- js/main.js
+++ js/main.js
@@ -1,32 +1,50 @@
-console.log("Movie Bot - main.js");
+console.log("Movie Bot - main3.js");
 
-// Make sure only movies with trailers/posters/overview are returned
-// Make sure no duplicates
-// Return in alphabetical Order
-// Apply clicked movie title into search bar
+// error return Message, no result
+// clear listMovies
 
-// Optimise for mobile
-// secure it
-
-var movieTrailer;
-var movieTitle;
-var movieOverview;
-var movieReleaseDate;
-var moviePosterId;
-var id;
+var moviesChecked = false;
 var inputFromUser;
-var newSearch = true;
+var moviesObject = [];
+var selectedMovie;
+var defaultResponse = {
+    results: [
+        {id: 299536},
+        {id: 329},
+        {id: 278},
+        {id: 155},
+        {id: 240},
+        {id: 284053},
+        {id: 424},
+        {id: 348350},
+        {id: 122},
+        {id: 550},
+        {id: 769},
+        {id: 603},
+        {id: 280},
+        {id: 9509},
+        {id: 2034},
+        {id: 157336},
+        {id: 14160},
+        {id: 107},
+        {id: 680},
+        {id: 312221}
+    ]
+}
+
+
 
     $(".search-movie").click(function(){
+        $(".search-result-status").empty();
+        moviesChecked = false;
+        moviesObject = [];
         inputFromUser = $("input:text").val();
-        newSearch = true;
         getMovieId(inputFromUser);
     });
 
-    $(".remove-video").click(function(){
-        $(".embed-responsive").empty();
+    $(".list-heading").click(function(){
+        defaultMovieList(defaultResponse);
     });
-
 
 var getMovieId = function(inputFromUser){
     var request = new XMLHttpRequest();
@@ -43,29 +61,15 @@ var getMovieId = function(inputFromUser){
     request.send();
 
     getElements = function(response) {
-        id = response;
-        if (newSearch){
-            getAllMovieTitles(id);
-            newSearch = false;
+        for (i = 0; i < response.results.length; i++) {
+            getMovieInfo(response.results[i].id);
+            if (i === response.results.length-1){
+                moviesChecked = true;
+                $(".search-result-status").append("<i>Search results:</i>");
+            }
         }
-        getMovieInfo(id.results[0].id);
     }
 };
-
-var getAllMovieTitles = function(){
-    $(".list-of-movies").empty();
-    var counter = id.results.length;
-
-    for (i = 0; i < counter; i++) { 
-        $(".list-of-movies").append("<a href='#/'><li data-val='"+ id.results[i].title +"'>" + id.results[i].title + "</li></a>");
-    }
-    $('li').click(function() {
-        // alert($(this).data('val'));
-        var selectedMovie = $(this).data('val');
-        getMovieId(selectedMovie);
-    });
-};
-
 
 var getMovieInfo = function(movieId){
 
@@ -83,38 +87,117 @@ var getMovieInfo = function(movieId){
     request.send();
 
     getElements = function(response) {
-        movieTrailer = response.videos.results[0].key;
-        movieTitle = response.title;
-        movieOverview = response.overview;
-        movieReleaseDate = response.release_date;
-        moviePosterId = response.poster_path;
-
-
-        console.log(movieTitle);
-        console.log(movieOverview);
-        console.log(movieReleaseDate);
-        console.log(response);
-
-        embedMovieDetailsHTML(movieTitle,movieReleaseDate,movieOverview);
-        embedMoviePosterHTML(moviePosterId);
-        embedVideoInHTML(movieTrailer);
+        makeMovieObject(response);
     }
 };
 
-var embedMovieDetailsHTML = function(movieTitle,movieReleaseDate,movieOverview){
-    $(".movie-details").empty();
-    $(".movie-details").append("<h2>"+ movieTitle + "</h2>" +
-                                movieOverview + "<br><br><p><i>Released: " + movieReleaseDate + "</i>"); 
-                                
+var makeMovieObject = function(response) {
+    function Movie(movieTitle, movieReleaseDate, movieOverview, moviePosterId, movieTrailer) {
+        this.movieTitle = movieTitle;
+        this.movieReleaseDate = movieReleaseDate;
+        this.movieOverview = movieOverview;
+        this.moviePosterId = moviePosterId;
+        this.movieTrailer = movieTrailer;
+        this.posterPath = function() {
+            return "https://image.tmdb.org/t/p/w300_and_h450_bestv2" + moviePosterId;
+        }
+        this.videoUrl = function() {
+            return "https://www.youtube.com/embed/" + movieTrailer;
+        }
+
+    };
+    if (response.videos.results[0]) {
+        moviesObject.push(new Movie(response.title, 
+                                    response.release_date, 
+                                    response.overview, 
+                                    response.poster_path,
+                                    response.videos.results[0].key
+                                    ));
+    } else {
+        return;
+    };
+    if (moviesChecked) {
+        listMovies(moviesObject);
+    };
 };
 
-var embedMoviePosterHTML = function(moviePosterId){
-    $(".movie-poster").empty();
-    $(".movie-poster").append("<img class='img-fluid' src='https://image.tmdb.org/t/p/w300_and_h450_bestv2" + moviePosterId + "'>");
+var listMovies = function(moviesObject){
+    $(".list-of-movies").empty();
+    var counter = moviesObject.length;
+        for (i = 0; i < counter; i++) { 
+            var counter = moviesObject.length;
+                $(".list-of-movies").append("<a href='#/'><li data-val='"+ i +"'>" + moviesObject[i].movieTitle + "</li></a>");
+        }
+
+    selectedMovie = 0;
+    renderMovie(selectedMovie);
+
+    $('li').click(function() {
+        selectedMovie = $(this).data('val');
+        renderMovie(selectedMovie);
+    });
 };
 
-var embedVideoInHTML = function(movieTrailer){
-    $(".embed-responsive").empty();
-    $(".embed-responsive").append("<iframe class='embed-responsive-item' src='https://www.youtube.com/embed/" + movieTrailer + "?rel=0' allowfullscreen></iframe>");
+var renderMovie = function(selectedMovie) {
+
+    var movieTitle = moviesObject[selectedMovie].movieTitle;
+    var movieReleaseDate = moviesObject[selectedMovie].movieReleaseDate;
+    var movieOverview = moviesObject[selectedMovie].movieOverview;
+    var moviePosterId = moviesObject[selectedMovie].posterPath();
+    var movieTrailer = moviesObject[selectedMovie].videoUrl();
+
+
+    var embedMovieDetailsHTML = function(movieTitle,movieReleaseDate,movieOverview){
+        $(".movie-details").empty();
+        $(".movie-details").append("<h2>"+ movieTitle + "</h2>" +
+                                    movieOverview + "<br><br><p><i>Released: " + movieReleaseDate + "</i>"); 
+                                    
+    };
+    
+    var embedMoviePosterHTML = function(moviePosterId){
+        $(".movie-poster").empty();
+        $(".movie-poster").append("<img class='img-fluid' src=" + moviePosterId + ">");
+    };
+    
+    var embedVideoInHTML = function(movieTrailer){
+        $(".embed-responsive").empty();
+        $(".embed-responsive").append("<iframe class='embed-responsive-item' src=" + movieTrailer + "?rel=0:1' allowfullscreen></iframe>");
+    };
+
+
+    embedMovieDetailsHTML(movieTitle,movieReleaseDate,movieOverview);
+    embedMoviePosterHTML(moviePosterId);
+    embedVideoInHTML(movieTrailer);
 };
+
+var defaultMovieList = function(response) {
+    $(".list-of-movies").empty();
+    $(".search-result-status").empty();
+    moviesObject = [];
+    for (i = 0; i < response.results.length; i++) {
+        getMovieInfo(response.results[i].id);
+        if (i === response.results.length-1){
+            moviesChecked = true;
+        }
+    }
+}
+
+defaultMovieList(defaultResponse);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
